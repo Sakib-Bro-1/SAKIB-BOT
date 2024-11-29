5ce3acf708e124b89eccbf006646fda3802cc115
Sakib-Bro-1 <darazaccout@gmail.com>
2024-11-29 15:37:17 +0600

    info.js

--- scripts/commands/info.js
+++ scripts/commands/info.js
@@ -40,7 +40,7 @@ var callback = () => api.sendMessage({body:`
 𝐆𝐦𝐚𝐢𝐥       : 𝐭𝐡𝐞𝐭𝐨𝐱𝐢𝐜𝐩𝐥𝐚𝐲𝐞𝐫𝟒𝟑𝟎@𝐠𝐦𝐚𝐢𝐥.𝐜𝐨𝐦
 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: 𝐡𝐭𝐭𝐩𝐬://𝐰𝐚.𝐦𝐞/+𝟖𝟖𝟎𝟏𝟕𝟔𝟓𝟎𝟓𝟏𝟐𝟏𝟗
 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  : 𝐡𝐭𝐭𝐩𝐬://𝐭.𝐦𝐞/𝐒𝐞𝐜𝐨𝐧𝐝𝐉𝐨𝐡𝐧𝐧𝐲𝐒𝐢𝐧𝐬
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : 𝐡𝐭𝐭𝐩𝐬://𝐟𝐚𝐜𝐞𝐛𝐨𝐨𝐤.𝐜𝐨𝐦/𝟐𝐧𝐝𝐉𝐨𝐡𝐧𝐧𝐲𝐒𝐢𝐧𝐬`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : 𝐡𝐭𝐭𝐩𝐬://𝐟𝐚𝐜𝐞𝐛𝐨𝐨𝐤.𝐜𝐨𝐦/𝟐𝐧𝐝𝐉𝐨𝐡𝐧𝐧𝐲𝐒𝐢𝐧𝐬 `,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
     fs.unlinkSync(__dirname + "/cache/1.png"));  
       return request(encodeURI(`https://scontent.fdac20-1.fna.fbcdn.net/v/t39.30808-6/466409816_858581113152347_6085690641774945827_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeG0lFTTvHECB62CvVF2T9YHf79hK3hWSIF_v2EreFZIgf1xiahOhiymuqb48dyZ012DAFoKsRSfRyORnVul9dva&_nc_ohc=AxPEQkOeNAUQ7kNvgFRm8PG&_nc_zt=23&_nc_ht=scontent.fdac20-1.fna&_nc_gid=AtCdymeYz6ElaGhGNvejPqg&oh=00_AYA8TMzxIt9f7apzuQU6noZnQ4n-sOh76di_n0fsC0P-DA&oe=674F6674`)).pipe(
 fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
